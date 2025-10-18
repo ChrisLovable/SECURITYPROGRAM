4dae6f5af6fde76e035220fc5338391ac7f26dda
chrisdevries <chrisdevries.personal@gmail.com>
2025-10-18 10:13:08 +0200

    Fix syntax error and prepare for deployment

--- src/pages/RosterPage.tsx
+++ src/pages/RosterPage.tsx
@@ -56,8 +56,6 @@ export default function RosterPage() {
     return cyclePosition < 60 ? 'ON' : 'OFF'
   }
 
-  }
-
   return (
     <div className="space-y-6">
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
